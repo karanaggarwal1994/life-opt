e5aed60cc1cf4a8c850317f23bbd2bdf469d164d
karanaggarwal1994 <karan.aggarwal61@gmail.com>
2020-01-30 07:41:31 +0530

    License update for CPU files

--- cpu-opt/bbnnls_cpu.m
+++ cpu-opt/bbnnls_cpu.m
@@ -1,3 +1,39 @@
+% This version was derived from the original version by Pestilli and Caiafa.
+% Copyright (2015), Franco Pestilli (Indiana Univ.) - Cesar F. Caiafa (CONICET)
+% email: pestillifranco@gmail.com and ccaiafa@gmail.com
+% https://github.com/francopestilli/life available under
+% https://github.com/francopestilli/life/blob/master/LICENSE.md
+% 
+% BSD 3-Clause License
+% 
+% Copyright (c) 2019, Karan Aggarwal (karan@iisc.ac.in)
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+% 
+% 1. Redistributions of source code must retain the above copyright notice, this
+%    list of conditions and the following disclaimer.
+% 
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+%    this list of conditions and the following disclaimer in the documentation
+%    and/or other materials provided with the distribution.
+% 
+% 3. Neither the name of the copyright holder nor the names of its
+%    contributors may be used to endorse or promote products derived from
+%    this software without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+% FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+% OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 function out = bbnnls_cpu(M, b, x0, opt, BLAS_PATH)
     % Few Initializations
     nCoeffs    = size(M.Phi.subs(:,1),1);
